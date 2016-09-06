34b53344c365bba147c1c875e2da099398f65655
juang nakarani <jxvn.nakarani@gmail.com>
2016-09-06 14:54:32 +0700

    perubahan package name

--- src/main/java/com/juang/servletjsp/Login.java
+++ src/main/java/com/juang/servletjsp/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.juang.basicservlet;
+package com.juang.servletjsp;
 
 import java.io.IOException;
 import javax.servlet.ServletConfig;
